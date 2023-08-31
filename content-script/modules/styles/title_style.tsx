c89e77a2ad585d0cc56f94cef97f5902d0a24e3e
Ich-mag-dich <san02@kakao.com>
2023-08-31 09:45:23 +0900

    title margin right 40px

--- content-script/modules/styles/title_style.tsx
+++ content-script/modules/styles/title_style.tsx
@@ -4,6 +4,7 @@ const TitleDiv = styled.div`
     text-align: left;
     padding-top: 25px;
     margin-left: 40px;
+    margin-right: 40px;
     font-size: 20px;
     font-weight: bold;
     font-family: Noto Sans CJK KR, sans-serif;
